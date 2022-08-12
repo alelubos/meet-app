71b295cc26953a5c1631aafd0632aaf7a3572506
alelubos <alelubos@gmail.com>
2022-08-12 02:56:09 +0200

    update WelcomeScreen.jsx link to Privacy Policy

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -28,7 +28,7 @@ function WelcomeScreen(props) {
         </div>
       </div>
       <a
-        href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
+        href="https://alelubos.github.io/meet-app/privacy.html"
         rel="nofollow noopener"
       >
         Privacy policy
